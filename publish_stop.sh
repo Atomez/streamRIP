46fc581721fb41e88b20dd9d7c47d77038142bb3
painteau <52933590+painteau@users.noreply.github.com>
2019-09-27 19:23:27 +0200

    Update publish_stop.sh

--- publish_stop.sh
+++ publish_stop.sh
@@ -32,11 +32,11 @@ if [[ -f $onpidfi ]]; then
 fi
 
 errcho "[$me] Starting offline stream..."
-if [ &backuptype = "stream" ] then 
+if [ $backuptype = "stream" ] then 
 ffmpeg -loglevel warning -i $rtmpB -c copy -f mpegts pipe:1 > $pfi &
 offpid=$!
 fi
-if [ &backuptype = "video" ] then 
+if [ $backuptype = "video" ] then 
 ffmpeg -loglevel warning -stream_loop -1 -re -i $offfi -c copy -f mpegts pipe:1 > $pfi &
 offpid=$!
 fi
